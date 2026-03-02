b866b7c2dc10dc5edafb28bc0f3074b9dcda8fc5
lau5365 <ziyulau8@gmail.com>
2026-03-02 16:13:02 +0800

    20260302 Update app and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 import Resume from "./resume";
-import Projects from "./resume2";
+import Resume2 from "./resume2";
+
 
 function App() {
   return (
-    <>
-      <Resume />
-      <Projects />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/resume2" element={<Resume2 />} />
+    </Routes>
   );
 }
 
