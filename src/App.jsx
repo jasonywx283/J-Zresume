783a10ca2d4e91149ec8c3107f997955e9b846da
lau5365 <ziyulau8@gmail.com>
2026-03-02 16:51:07 +0800

    20260302 merge together both

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import React, { useContext } from "react";
-import Resume from "./Resume";       // 注意文件名大小写
-import Projects from "./Resume2";    // 注意文件名大小写
+import Resume from "./Resume";        // Make sure filename matches exactly
+import Resume2 from "./Resume2";      // Rename Projects to Resume2 for consistency
 import { Box, IconButton } from "@mui/material";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import { useTheme } from "@mui/material/styles";
 import { ColorModeContext } from "./ThemeContext";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function ModeToggle() {
   const theme = useTheme();
@@ -40,9 +42,14 @@ function App() {
       {/* Day/Night Mode Toggle Button */}
       <ModeToggle />
 
-      {/* 主内容 */}
-      <Resume />
-      <Projects />
+      {/* Routing */}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/resume2" element={<Resume2 />} />
+        {/* Add a default route if needed */}
+        <Route path="*" element={<Home />} />
+      </Routes>
     </Box>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import ThemeContextProvider from "./ThemeContext";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <ThemeContextProvider>
-      <App />
+    <App />
     </ThemeContextProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
